275741e3d562286cbefd8506436afa602abe7683
chriskang24 <christopherjkang24@gmail.com>
2021-02-16 14:37:43 -0500

    InterviewerList complete

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+var classNames = require('classnames');
+
+export default function InterviewerList(props) {
+
+  const { interviewers } = props;
+
+  const parsedInterviewers = interviewers.map(interviewer =>
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+      />
+    )
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewers}</ul>
+    </section>
+  );
+}
