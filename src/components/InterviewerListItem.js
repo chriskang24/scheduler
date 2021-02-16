42198ed9c2be9d14f0a622b55df31cd2dddd22e8
chriskang24 <christopherjkang24@gmail.com>
2021-02-16 13:52:28 -0500

    building interview item complete

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
 
+var classNames = require('classnames');
+
 export default function InterviewerListItem(props) {
 
+  const interviewClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewClass} onClick={() => props.setInterviewer(props.name)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-  Sylvia Palmer
+      {props.name}
     </li>
   );
 }
