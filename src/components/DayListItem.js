989f87112d26ee3f2a417bd6e04c507600e1ff80
chriskang24 <christopherjkang24@gmail.com>
2021-02-16 13:29:13 -0500

    push day 1 complete work

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+var classNames = require('classnames');
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  const formatSpots = function(spots) {
+    let spotsRemaining;
+
+    if (spots === 0) {
+      spotsRemaining = `no spots remaining`
+    } else if (spots === 1) {
+      spotsRemaining = `${spots} spot remaining`
+    } else {
+      spotsRemaining = `${spots} spots remaining`
+    }
+
+    return spotsRemaining;
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text-light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
+
