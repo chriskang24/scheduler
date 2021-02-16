14a86daee47cb44b01dd90f6a057587b7ca97b54
chriskang24 <christopherjkang24@gmail.com>
2021-02-16 14:59:32 -0500

    refactor prop drilling to interviewerList

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,18 +5,18 @@ var classNames = require('classnames');
 
 export default function InterviewerListItem(props) {
 
-  const interviewClass = classNames("interviewers__item", {
+  const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
   });
 
   return (
-    <li className={interviewClass} onClick={() => props.setInterviewer(props.name)}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   );
 }
