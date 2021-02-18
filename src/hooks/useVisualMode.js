2f581693fbeb8d37bf8542bb75d3b2a589936d93
chriskang24 <christopherjkang24@gmail.com>
2021-02-18 15:53:58 -0500

    initial build custom hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (nextMode, replace = false) => {
+
+    if (replace) {
+      const newArr = [...history]
+      newArr[0] = nextMode
+      setHistory(newArr)
+    } else {
+    
+    setHistory(prev => [nextMode, ...prev]);
+    }
+    
+  }
+
+  const back = () => {
+
+    setHistory(prev => prev.length > 1 ? prev.slice(1) : prev)
+  }
+
+  return { mode: history[0], transition, back };
+}
+
+
